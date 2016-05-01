9601ac0b99442966283c69a64f9b909a60129011
michaeltangelo <tanman24689@gmail.com>
2016-05-01 18:50:59 -0400

    initial commit -- demo workinggit status

---
+++ SpewDemo.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class SpewDemo {
+    public static void main(String[] args) {
+        int[] a = {1,2,3,4,5};
+
+        ArrayList<Integer> b = new ArrayList<Integer>();
+        b.add(1);
+        b.add(2);
+        b.add(3);
+        b.add(4);
+        b.add(5);
+
+        for(i = 0; i < 30; i++) {}
